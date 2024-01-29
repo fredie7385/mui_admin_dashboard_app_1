1dfb675f4c253328fabdf0c28ca28fb3d46fc29e
Fredrick Kabiru <50455667+fredie7385@users.noreply.github.com>
2024-01-29 17:31:21 +0300

    subject: components components in one file

--- src/components/Components.jsx
+++ src/components/Components.jsx
@@ -1,2 +1,4 @@
 export {default as SideNav} from './SideNav';
-export {default as AppHeader} from './AppHeader'
+export {default as AppHeader} from './AppHeader';
+export {default as LatestVideoCard} from './LatestVideoCard';
+export {default as ColorText} from './ColorText.jsx';
