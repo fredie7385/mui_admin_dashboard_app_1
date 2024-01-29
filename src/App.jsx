1ffbd6a959963a9b2fabd0d4269796b7db222de9
Fredrick Kabiru <50455667+fredie7385@users.noreply.github.com>
2024-01-29 17:48:50 +0300

    subject: add BrowserRouter to the app. Add AppRoutes component in the main component..

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { Box, CssBaseline, ThemeProvider } from "@mui/material";
 import React from "react";
 import theme from "./config/Theme";
 import { SideNav, AppHeader } from "./components/Components";
-
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from './router/AppRoutes.jsx';
 function App() {
   return (
     <React.Fragment>
@@ -11,8 +12,13 @@ function App() {
         <CssBaseline />
         <AppHeader />
         <Box sx={styles.container}>
-          <SideNav />
-          <Box component={"main"} sx={styles.mainSection}></Box>
+          {/* helps in adding routers in the app */}
+          <BrowserRouter>
+            <SideNav />
+            <Box component={"main"} sx={styles.mainSection}>
+              <AppRoutes/>
+            </Box>
+          </BrowserRouter>
         </Box>
       </ThemeProvider>
     </React.Fragment>
@@ -23,12 +29,12 @@ function App() {
 /**@type {import('@mui/material').SxProps } */
 const styles = {
   container: {
-    bgcolor: "neutral.light",
+    bgcolor: "neutral.medium",
     display: "flex",
     height: "calc(100% - 64px)",
   },
   mainSection: {
-    p: 1,
+    p: 3,
     width: "100%",
     height: "100%",
     overflow: "auto",
