0da00087b4cdd2f2c4b3de59b02532140d76f3e5
Fredrick Kabiru <50455667+fredie7385@users.noreply.github.com>
2024-01-29 09:26:26 +0300

    feat: app theme provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
-import { ThemeProvider, Typography } from "@mui/material"
-import React from "react"
-import theme from "./config/Theme"
-
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import React from "react";
+import theme from "./config/Theme";
+import { SideNav, AppHeader } from "./components/Components";
 
 function App() {
   return (
     <React.Fragment>
       <ThemeProvider theme={theme}>
-<Typography>
-  hello
-</Typography>
+        {/* The rest of your application */}
+        <CssBaseline />
+        <AppHeader />
+        <Box sx={styles.container}>
+          <SideNav />
+          <Box component={"main"} sx={styles.mainSection}></Box>
+        </Box>
       </ThemeProvider>
     </React.Fragment>
-  )
+  );
 }
 
-export default App
+// helps in vscode autocompletion for MUI
+/**@type {import('@mui/material').SxProps } */
+const styles = {
+  container: {
+    bgcolor: "neutral.light",
+    display: "flex",
+    height: "calc(100% - 64px)",
+  },
+  mainSection: {
+    p: 1,
+    width: "100%",
+    height: "100%",
+    overflow: "auto",
+  },
+};
+
+export default App;
