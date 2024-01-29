3aebd965ac50f6de27082a899fa5bd30cb0f1559
Fredrick Kabiru <50455667+fredie7385@users.noreply.github.com>
2024-01-29 17:30:38 +0300

    subject:  container components

---
+++ src/containers/Container.jsx
@@ -0,0 +1,5 @@
+export{default as Dashboard}  from './Dashboard/Dashboard';
+export {default as Content} from './Content/Content';
+export{default as Customization} from './Customization/Customization';
+export{default as Analytics} from './analytics/Analytic';
+
