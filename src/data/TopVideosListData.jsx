c36ef48dd546973b4b62eba2ceb27cdd1dad33cd
Fredrick Kabiru <50455667+fredie7385@users.noreply.github.com>
2024-02-07 16:43:23 +0300

    subject: putting data on different file.

---
+++ src/data/TopVideosListData.jsx
@@ -0,0 +1,38 @@
+export const rows = [
+  {
+    thumbnail: "src/assets/thumbnail.png",
+    name: "How to become a software developer",
+    averageViewDuration: "2:25 (41.0%)",
+    views: 3211,
+  },
+  {
+    thumbnail: "src/assets/thumbnail2.png",
+    name: "React Native Crash Course in 4 hours",
+    averageViewDuration: "9:45 (30.5%)",
+    views: 3102,
+  },
+  {
+    thumbnail: "src/assets/thumbnail.png",
+    name: "ReactJs SearchBar in 5 minutes",
+    averageViewDuration: "3:10 (25.5%)",
+    views: 1900,
+  },
+  {
+    thumbnail: "src/assets/thumbnail2.png",
+    name: "JavaScript Crash Course including real world projects",
+    averageViewDuration: "8:10 (15.5%)",
+    views: 1201,
+  },
+  {
+    thumbnail: "src/assets/thumbnail2.png",
+    name: "How to build an admin panel using React and TypeScript",
+    averageViewDuration: "2:10 (34.1%)",
+    views: 1195,
+  },
+  {
+    thumbnail: "src/assets/thumbnail.png",
+    name: "TypeScript Crash Course for beginners",
+    averageViewDuration: "5:20 (14.1%)",
+    views: 1037,
+  },
+];
